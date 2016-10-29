24a2b0b7f17bc42ef696ccc58e95932f9fa57a1c
Ben Gamari <ben@smart-cactus.org>
2016-10-29 15:35:50 -0400

    Update

--- default.nix
+++ default.nix
@@ -1,7 +1,9 @@
-{ system ? builtins.currentSystem }:
+{ pkgs ? import <nixpkgs> {}; }:
+#{ system ? builtins.currentSystem }:
 
 let
-  pkgs = import <nixpkgs> { inherit system; };
+  #pkgs = import <nixpkgs> { inherit system; };
+  bits = "64";
 in
   rec {
     llvm-riscv = import ./llvm-riscv.nix {
@@ -9,12 +11,33 @@ in
       inherit (pkgs) stdenv perl groff cmake python libffi binutils libxml2 valgrind ncurses zlib compiler-rt_src libcxxabi;
     };
 
+    riscv-qemu = import ./riscv-qemu.nix {
+      inherit (pkgs) stdenv autoconf automake libtool flex bison SDL zlib glib pkgconfig python;
+    };
+
     riscv-fesvr = import ./riscv-fesvr.nix {
       inherit (pkgs) stdenv;
     };
 
-    riscv-gnu-toolchain = import ./riscv-gnu-toolchain.nix {
-      inherit (pkgs) stdenv gmp mpfr libmpc wget curl texinfo bison flex;
+    riscv-binutils = import ./riscv-binutils.nix {
+      inherit (pkgs) stdenv texinfo flex bison;
+      inherit bits;
+    };
+
+    riscv-glibc = import ./riscv-glibc.nix {
+      inherit (pkgs) stdenv gmp mpfr libmpc;
+      inherit bits riscv-gcc-stage1;
+    };
+
+    riscv-gcc-stage1 = import ./riscv-gcc-stage1.nix {
+      inherit (pkgs) stdenv texinfo gmp mpfr libmpc flex bison;
+      inherit bits riscv-binutils;
+    };
+
+    riscv-gcc = import ./riscv-gcc.nix {
+      inherit (pkgs) stdenv texinfo gmp mpfr libmpc flex bison;
+      inherit bits riscv-binutils;
+      glibCross = riscv-glibc;
     };
 
     riscv-pk = import ./riscv-pk.nix {
@@ -28,5 +51,23 @@ in
 
     linux-riscv = import ./linux-riscv.nix {
       inherit (pkgs) stdenv;
+    };
+
+    # These are broken
+    riscv-gnu-toolchain = import ./riscv-gnu-toolchain.nix {
+      inherit (pkgs) stdenv gmp mpfr libmpc wget curl texinfo bison flex;
+    };
+
+    binutils-riscv = (pkgs.binutils.overrideDerivation (oldAttrs: {
+      src = pkgs.fetchgit {
+        rev = "67561745546973c1e969348e274129b2d0637b1c";
+        url = "git://github.com/riscv/riscv-binutils-gdb.git";
+        sha256 = "0fhniqqvh6hn31xnrsdjyikaklp21r718f2kxph4gkx9ma88b9wn";
+      };
+      dontDisableStatic = true;
+    })).override {
+      cross = {
+        config = "riscv64-unknown-linux";
+      };
     };
   }
--- riscv-pk.nix
+++ riscv-pk.nix
@@ -7,7 +7,7 @@ in
     name = "riscv-pk";
     src = nixpkgs.fetchgit {
       rev = "85ae17aa149b9ea114bdd70cc30ea7e73813fb48";
-      url = "git://github.com:riscv/riscv-pk.git";
+      url = "git://github.com/riscv/riscv-pk.git";
       sha256 = "0jlj0vhmxxl2w9xjzhdcl4k1p3dgq93npqpqkjg7qcngpjlmifd2";
     };
   }

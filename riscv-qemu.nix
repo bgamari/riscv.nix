df7a377ea171a4feabb418b25ada9c2754cc06da
Ben Gamari <ben@smart-cactus.org>
2016-11-11 20:08:39 -0500

    Update riscv-qemu

--- riscv-qemu.nix
+++ riscv-qemu.nix
@@ -12,8 +12,8 @@ in
       "--target-list=riscv64-softmmu,riscv32-softmmu,riscv64-linux-user,riscv32-linux-user"
     ];
     src = nixpkgs.fetchgit {
-      rev = "05ac735a4b4085e39678c967ecf7f47d45f459e0";
+      rev = "b12cfa344681687c7adbc5fa66590182df3748b9";
       url = "git://github.com/riscv/riscv-qemu.git";
-      sha256 = "06c2sww4nqbxnk6i7ndc2qyykv1r4mcfbnmwi0wv1vklkvpcnb9v";
+      sha256 = "15pcl936ppfk57bylfyshhz01yink1khz54iwispa1g64smfn803";
     };
   }
